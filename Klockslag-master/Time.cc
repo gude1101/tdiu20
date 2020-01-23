818c63208760e3ff647cc8ee72368626f050c5af
gusde001 <gusde001@su14-108.ad.liu.se>
2020-01-23 13:18:50 +0100

    first

---
+++ Klockslag-master/Time.cc
@@ -0,0 +1,8 @@
+// I denna fil läggs definitionerna (implementationen) av de funktioner
+// som deklarerats i Time.h
+
+#include Time.h
+
+using namespace std;
+
+Time::
---
+++ Klockslag-master/Time.h
@@ -0,0 +1,9 @@
+// Denna fil ska innehålla deklarationer för de typer och funktioner
+// som behövs
+
+class Time {
+ public:
+  int hour();
+  int minute();
+  int second();
+}
