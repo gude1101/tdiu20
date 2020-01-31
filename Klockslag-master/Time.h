2dd1084169149dc3037c5aaf0a4663c337b74b3d
gusdet <48828259+gude1101@users.noreply.github.com>
2020-01-31 11:24:20 +0100

    Add files via upload

--- Klockslag-master/Time.h
+++ Klockslag-master/Time.h
@@ -9,12 +9,15 @@ class Time {
  public:
   Time();
   Time(int h, int m, int s);
-  
+  Time(std::string const & s);
+
   int hour();
   int minute();
   int second();
 
-  std::string to_string();
+  bool is_am();
+
+  std::string to_string(bool am_pm = false);
  private:
   int m_hour;
   int m_minute;
