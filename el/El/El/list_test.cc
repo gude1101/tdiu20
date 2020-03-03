1f716cd978a5fe871f26671d7f7a798e4851d98e
gusdet <48828259+gude1101@users.noreply.github.com>
2020-03-03 09:53:44 +0100

    merge

---
+++ el/El/El/list_test.cc
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "catch.hpp"
+//#include ".h"
+
+using namespace std;
+
+// här lägger ni era testfall.
+// Jobba enligt TDD;
+//  1. Lägg till testfall
+//  2. Testa
+//  3. Lägg till (minsta möjliga) implementation
+//  4. Testa alla testfall
+//  5. Refaktorera (skriv om) så att allt ser bra ut
+
+#if 01
+
+TEST_CASE("Empty constructor")
+{
+	CHECK(2-1 == 1);
+}
+#endif
