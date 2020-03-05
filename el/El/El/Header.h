8e3071c1df6f911ce7832b36f1c03f83912880cc
gusdet <48828259+gude1101@users.noreply.github.com>
2020-03-05 12:55:50 +0100

    merge

--- el/El/El/Header.h
+++ el/El/El/Header.h
@@ -1,1 +1,26 @@
 #pragma once
+
+class Connection {
+
+};
+
+class Component {
+	Connection _from;
+	Connection _to;
+
+	Component(Connection from, Connection to) : _from{ from }, _to{ to } {
+
+	}
+};
+
+class Resistor : public Component {
+
+};
+
+class Battery : public Component {
+
+};
+
+class Capacitor : public Component {
+
+};
