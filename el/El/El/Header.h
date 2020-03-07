78ffa62b119d0caf5dba6669ba90c20176150aa4
Sam Bell <gustav@detterfelt.se>
2020-03-07 23:17:23 +0100

    merge

--- el/El/El/Header.h
+++ el/El/El/Header.h
@@ -9,7 +9,7 @@ void simulate(std::vector<Component*> net, int iterations, double voltage, doubl
 
 class Connection {
 public:
-	double Potential;
+	double Potential = 0;
 };
 
 class Component {
@@ -21,20 +21,32 @@ protected:
 public:
 	Component(std::string name, double special_val, Connection from, Connection to);
 
-	virtual void simulate() = 0; // abstract
+	virtual void simulate(double delta_t) = 0;
+	virtual double get_current() = 0;
+	double get_voltage();
 };
 
+std::ostream operator<<(std::ostream & os, Component & c);
+
 class Battery : public Component {
+public:
 	using Component::Component;
-	void simulate();
+	void simulate(double delta_t);
+	double get_current();
 };
 
 class Resistor : public Component {
+public:
 	using Component::Component;
-	void simulate();
+	void simulate(double delta_t);
+	double get_current();
 };
 
 class Capacitor : public Component {
+private:
+	double _charge;
+public:
 	using Component::Component;
-	void simulate();
+	void simulate(double delta_t);
+	double get_current();
 };
--- el/El/El/Implementation.cc
+++ el/El/El/Implementation.cc
@@ -1,13 +1,52 @@
 #include "Header.h"
 
+using namespace std;
+
 Component::Component(std::string name, double special_val, Connection from, Connection to)
 	: _name{ name }, _special_val{ special_val }, _from{ from }, _to{ to } {}
 
-void Battery::simulate() {
+double Component::get_voltage() {
+	return _from.Potential - _to.Potential;
+}
+
+ostream operator<<(ostream& os, Component& c) {
+	os
+		<< cout.width(5) << cout.precision(2)
+		<< c.get_voltage() << " " << c.get_current();
+}
+
+
+double Battery::get_current() {
+	return 0;
+}
+
+void Battery::simulate(double delta_t) {
 	_from.Potential = 0;
 	_to.Potential = _special_val;
 }
 
-void Resistor::simulate() {
-	double voltage = (_to.Potential - _from.Potential) * 
+
+double Resistor::get_current() {
+	return get_voltage() / _special_val;
+}
+
+void Resistor::simulate(double delta_t) {
+	double x = _to.Potential - _from.Potential;
+	x /= _special_val;
+	x *= delta_t;
+
+	_to.Potential += x;
+	_from.Potential -= x;
+}
+
+
+double Capacitor::get_current() {
+	return _special_val * (get_voltage() - _charge);
+}
+
+void Capacitor::simulate(double delta_t) {
+	double x = get_current() * delta_t;
+	_from.Potential -= x;
+	_charge += x;
+	_to.Potential += x;
 }
